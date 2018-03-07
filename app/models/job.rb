6e9f95d8920d1a89ed9e250b9d4f192d1038efb0
Sammy Watchman <swsw872@gmail.com>
2018-03-06 17:34:52 -0700

    styled some pages to make more user friendly

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,6 @@ class Job < ApplicationRecord
   belongs_to :user
   has_many :boat_jobs, dependent: :destroy
   has_many :boats, through: :boat_jobs
+
+  validates :cost, presence: true, numericality: {minimum: 1000}
 end
