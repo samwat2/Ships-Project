d079a12c9049780823e2962f4f30b1a4ecc500c6
Sammy Watchman <swsw872@gmail.com>
2018-03-19 17:58:01 -0600

    added validations and new selection options for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,6 @@ class Job < ApplicationRecord
   has_many :boats, through: :boat_jobs
 
   validates :cost, presence: true, numericality: { greater_than: 1000 } 
+  validates :description, uniqueness: true, presence: true, length: { minimum: 50 }
+  validates_inclusion_of :location, in: ['Sweden', 'USA', 'China', 'Thailand', 'Norway']
 end
